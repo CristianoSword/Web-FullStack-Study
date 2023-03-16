bc0caca1f28c56f83a1cb4f7c27b0b45f55300ee
cristianoSword <originalvgo@gmail.com>
2023-03-16 11:27:15 -0300

    webpack config

---
+++ 1-FrontEnd/2-Javascript/webpack-tuto/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './index.js')
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: path.resolve(__dirname, './template.html'), // template file
+          filename: 'index.html', // output file
+        }),
+      ],
+}
