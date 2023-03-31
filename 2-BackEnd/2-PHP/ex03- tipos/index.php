049d7309fee6d5ef9e4e007343a6300acbc32b84
cristianoSword <originalvgo@gmail.com>
2023-03-31 17:14:56 -0300

    php8 tipoa up1

---
+++ 2-BackEnd/2-PHP/ex03- tipos/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos</title>
+</head>
+<body>
+    <h1>Tipos primitivos</h1>
+    <?php
+        $num = 341;
+        echo "o valor de num é $num";
+        
+
+        $num2 = (int)3.14;
+        // var_dump($num);
+        echo "<br>";
+        var_dump($num2);
+        echo "<br>";
+        $casado = false;
+        print "a var casado tem o valor de $casado"; //false emecho/print sao apresentados como nulo ou vazio
+    ?>
+</body>
+</html>
