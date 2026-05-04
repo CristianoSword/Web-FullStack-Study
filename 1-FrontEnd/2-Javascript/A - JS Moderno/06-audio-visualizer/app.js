3cfa9af66e9f557455760707165ac3a2fa8892bb
Cristiano Camacho <originalvgo@gmail.com>
2026-05-04 01:15:00 -0300

    feat: renderização em tempo real e finalização do Audio Visualizer

--- 1-FrontEnd/2-Javascript/A - JS Moderno/06-audio-visualizer/app.js
+++ 1-FrontEnd/2-Javascript/A - JS Moderno/06-audio-visualizer/app.js
@@ -66,4 +66,33 @@ playPauseBtn.addEventListener('click', () => {
     isPlaying = !isPlaying;
 });
 
+function animate() {
+    if (!isPlaying) return;
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.getByteFrequencyData(dataArray);
+
+    const barWidth = (canvas.width / bufferLength) * 2.5;
+    let barHeight;
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i] * 2;
+
+        const r = barHeight + (25 * (i / bufferLength));
+        const g = 250 * (i / bufferLength);
+        const b = 50;
+
+        ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+
+        x += barWidth + 1;
+    }
+
+    requestAnimationFrame(animate);
+}
+
 export { canvas, ctx, analyser, audio };
