404e31cf970b6dfa67c834b44e87dd2bf6a52681
Cristiano Camacho <originalvgo@gmail.com>
2026-05-12 20:15:29 -0300

    04-WeatherHub: [03/08] core

---
+++ 1-FrontEnd/9-TypeScript/A - TypeScript basico/04-WeatherHub/src/service.ts
@@ -0,0 +1,43 @@
+import { WeatherData, WeatherApiResponse, WeatherCondition } from "./types";
+
+export class WeatherService {
+    // Simula uma chamada de API
+    async fetchWeather(city: string): Promise<WeatherData> {
+        console.log(`Buscando clima para ${city}...`);
+        
+        // Simula resposta da API
+        const mockResponse: WeatherApiResponse = {
+            name: city,
+            main: { temp: 25, humidity: 60 },
+            weather: [{ main: "Clear" }],
+            wind: { speed: 10 }
+        };
+
+        return this.mapResponseToData(mockResponse);
+    }
+
+    private mapResponseToData(res: WeatherApiResponse): WeatherData {
+        return {
+            city: res.name,
+            temperature: res.main.temp,
+            humidity: res.main.humidity,
+            windSpeed: res.wind.speed,
+            condition: this.mapCondition(res.weather[0].main)
+        };
+    }
+
+    private mapCondition(main: string): WeatherCondition {
+        switch (main.toLowerCase()) {
+            case "clear": return WeatherCondition.SUNNY;
+            case "clouds": return WeatherCondition.CLOUDY;
+            case "rain": return WeatherCondition.RAINY;
+            case "snow": return WeatherCondition.SNOWY;
+            default: return WeatherCondition.SUNNY;
+        }
+    }
+
+    // Type Guard
+    isWeatherData(data: any): data is WeatherData {
+        return (data as WeatherData).city !== undefined;
+    }
+}
