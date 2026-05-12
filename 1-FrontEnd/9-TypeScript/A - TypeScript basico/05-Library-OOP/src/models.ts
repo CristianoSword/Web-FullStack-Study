30091fa994ba805a0583555d52ef4037d8e0ac80
Cristiano Camacho <originalvgo@gmail.com>
2026-05-12 20:16:53 -0300

    05-Library-OOP: [02/08] types

---
+++ 1-FrontEnd/9-TypeScript/A - TypeScript basico/05-Library-OOP/src/models.ts
@@ -0,0 +1,48 @@
+export abstract class BaseItem {
+    constructor(
+        public readonly id: string,
+        public title: string,
+        protected isAvailable: boolean = true
+    ) {}
+
+    abstract getDetails(): string;
+
+    toggleAvailability(): void {
+        this.isAvailable = !this.isAvailable;
+    }
+
+    get availability(): boolean {
+        return this.isAvailable;
+    }
+}
+
+export class Book extends BaseItem {
+    constructor(
+        id: string,
+        title: string,
+        public author: string,
+        public pages: number
+    ) {
+        super(id, title);
+    }
+
+    getDetails(): string {
+        return `Livro: ${this.title} por ${this.author} (${this.pages} pgs)`;
+    }
+}
+
+export class Member {
+    constructor(
+        public id: string,
+        public name: string,
+        private borrowedItems: BaseItem[] = []
+    ) {}
+
+    borrow(item: BaseItem): void {
+        this.borrowedItems.push(item);
+    }
+
+    getBorrowedItems(): BaseItem[] {
+        return this.borrowedItems;
+    }
+}
