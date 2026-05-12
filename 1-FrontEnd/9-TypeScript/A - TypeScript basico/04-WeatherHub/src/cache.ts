e5624888932939dec5a0299a467fa2315cf7a65b
Cristiano Camacho <originalvgo@gmail.com>
2026-05-12 20:15:46 -0300

    04-WeatherHub: [05/08] data

---
+++ 1-FrontEnd/9-TypeScript/A - TypeScript basico/04-WeatherHub/src/cache.ts
@@ -0,0 +1,22 @@
+import { WeatherData } from "./types";
+
+export class WeatherCache {
+    private cache = new Map<string, { data: WeatherData, expiry: number }>();
+    private TTL = 60 * 1000; // 1 minuto
+
+    set(city: string, data: WeatherData): void {
+        this.cache.set(city, {
+            data,
+            expiry: Date.now() + this.TTL
+        });
+    }
+
+    get(city: string): WeatherData | null {
+        const item = this.cache.get(city);
+        if (item && item.expiry > Date.now()) {
+            return item.data;
+        }
+        this.cache.delete(city);
+        return null;
+    }
+}
