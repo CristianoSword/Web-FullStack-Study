502fd3fcdb27067c2977457267188a2e2567a7d2
Cristiano Camacho <originalvgo@gmail.com>
2026-05-12 20:18:37 -0300

    06-EventBus: [03/08] core

---
+++ 1-FrontEnd/9-TypeScript/A - TypeScript basico/06-EventBus/src/bus.ts
@@ -0,0 +1,26 @@
+import { EventKey, EventMap, EventHandler } from "./types";
+
+export class EventBus {
+    private listeners: { [K in EventKey]?: EventHandler<K>[] } = {};
+
+    on<K extends EventKey>(key: K, handler: EventHandler<K>): void {
+        if (!this.listeners[key]) {
+            this.listeners[key] = [];
+        }
+        this.listeners[key]!.push(handler);
+    }
+
+    emit<K extends EventKey>(key: K, payload: EventMap[K]): void {
+        const handlers = this.listeners[key];
+        if (handlers) {
+            handlers.forEach(handler => handler(payload));
+        }
+    }
+
+    off<K extends EventKey>(key: K, handler: EventHandler<K>): void {
+        const handlers = this.listeners[key];
+        if (handlers) {
+            this.listeners[key] = handlers.filter(h => h !== handler);
+        }
+    }
+}
