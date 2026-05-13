0c162349e7dc007693d9a761e6cc6c19fe1c0447
Cristiano Camacho <originalvgo@gmail.com>
2026-05-13 00:20:44 -0300

    03-ReactiveState: [03/09] core-logic

---
+++ 1-FrontEnd/9-TypeScript/B - TypeScript Avancado/03-ReactiveState/src/signals.ts
@@ -0,0 +1,38 @@
+import { Subscriber } from "./types";
+
+let context: Subscriber[] = [];
+
+export function signal<T>(initialValue: T) {
+    let value = initialValue;
+    const subscribers = new Set<Subscriber>();
+
+    return {
+        get value() {
+            const currentSubscriber = context[context.length - 1];
+            if (currentSubscriber) {
+                subscribers.add(currentSubscriber);
+            }
+            return value;
+        },
+        set value(newValue: T) {
+            value = newValue;
+            subscribers.forEach(sub => sub());
+        },
+        subscribe(sub: Subscriber) {
+            subscribers.add(sub);
+            return () => subscribers.delete(sub);
+        }
+    };
+}
+
+export function effect(fn: Subscriber) {
+    const subscriber = () => {
+        context.push(subscriber);
+        try {
+            fn();
+        } finally {
+            context.pop();
+        }
+    };
+    subscriber();
+}
