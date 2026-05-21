dbfed98a17e8c63b5fd7dbaa755e88d2e98ac70a
Cristiano Camacho <originalvgo@gmail.com>
2026-05-21 14:30:28 -0300

    feat(04-E-Commerce-SPA): [05/06] validation/fixes

--- 1-FrontEnd/5-Vue/B - Vue avançado/04-E-Commerce-SPA/src/App.vue
+++ 1-FrontEnd/5-Vue/B - Vue avançado/04-E-Commerce-SPA/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="store-app-root">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
