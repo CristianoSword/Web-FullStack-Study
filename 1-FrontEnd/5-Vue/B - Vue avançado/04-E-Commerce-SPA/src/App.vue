0c5a38feff7b0fdaf6e9c3cca4051851fe9bc08b
Cristiano Camacho <originalvgo@gmail.com>
2026-05-21 14:29:12 -0300

    feat(04-E-Commerce-SPA): [01/06] setup

---
+++ 1-FrontEnd/5-Vue/B - Vue avançado/04-E-Commerce-SPA/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="store-app-root">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// Root App Component
+</script>
+
+<style scoped>
+.store-app-root {
+  min-height: 100vh;
+  background-color: var(--bg-app);
+}
+</style>
