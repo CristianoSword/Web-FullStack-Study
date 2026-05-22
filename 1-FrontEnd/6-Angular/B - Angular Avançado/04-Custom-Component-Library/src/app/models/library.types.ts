ee1e1bd62b03fa96ae562ea7e41019f18692bac5
Cristiano Camacho <originalvgo@gmail.com>
2026-05-22 13:49:47 -0300

    feat(04-Custom-Component-Library): [02/06] types/models

---
+++ 1-FrontEnd/6-Angular/B - Angular Avançado/04-Custom-Component-Library/src/app/models/library.types.ts
@@ -0,0 +1,10 @@
+export type AuraButtonVariant = 'primary' | 'secondary' | 'outline' | 'danger';
+
+export type AuraBadgeVariant = 'success' | 'warning' | 'info' | 'danger';
+
+export interface AuraCardConfig {
+  title?: string;
+  subtitle?: string;
+  hoverable?: boolean;
+  clickable?: boolean;
+}
