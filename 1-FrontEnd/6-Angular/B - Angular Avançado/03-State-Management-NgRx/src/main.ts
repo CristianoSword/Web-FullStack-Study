4959c6aeca2acb1c6d732dc875aaeb15855e507c
Cristiano Camacho <originalvgo@gmail.com>
2026-05-22 13:46:55 -0300

    feat(03-State-Management-NgRx): [04/06] ui/routes

--- 1-FrontEnd/6-Angular/B - Angular Avançado/03-State-Management-NgRx/src/main.ts
+++ 1-FrontEnd/6-Angular/B - Angular Avançado/03-State-Management-NgRx/src/main.ts
@@ -1,4 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
+import { cartReducer } from './app/store/cart.reducer';
 
-bootstrapApplication(AppComponent).catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideStore({ cart: cartReducer })
+  ]
+}).catch(err => console.error(err));
