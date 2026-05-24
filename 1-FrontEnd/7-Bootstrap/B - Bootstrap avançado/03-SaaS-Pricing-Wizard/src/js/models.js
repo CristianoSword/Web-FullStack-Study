86bfe82ac0c14d4d6e030874b1302d6f63addced
Cristiano Camacho <originalvgo@gmail.com>
2026-05-24 11:50:31 -0300

    feat(03-SaaS-Pricing-Wizard): [02/06] types/models

---
+++ 1-FrontEnd/7-Bootstrap/B - Bootstrap avançado/03-SaaS-Pricing-Wizard/src/js/models.js
@@ -0,0 +1,40 @@
+export const PLANS = [
+  {
+    id: "plan-starter",
+    name: "Starter",
+    monthlyPrice: 29.00,
+    annualPrice: 24.00,
+    features: [
+      "1 Usuário",
+      "5 Projetos Ativos",
+      "Suporte por E-mail"
+    ],
+    popular: false
+  },
+  {
+    id: "plan-pro",
+    name: "Professional",
+    monthlyPrice: 79.00,
+    annualPrice: 64.00,
+    features: [
+      "5 Usuários",
+      "Projetos Ilimitados",
+      "Suporte 24/7 de Elite",
+      "Acesso à API Aura"
+    ],
+    popular: true
+  },
+  {
+    id: "plan-enterprise",
+    name: "Enterprise",
+    monthlyPrice: 199.00,
+    annualPrice: 159.00,
+    features: [
+      "Usuários Ilimitados",
+      "Segurança Dedicada",
+      "Auditorias Customizadas",
+      "Consultoria de Infra"
+    ],
+    popular: false
+  }
+];
