2a52008b809f5cc2e222a94be60bcaae6d334bfc
Cristiano Camacho <originalvgo@gmail.com>
2026-05-23 22:52:43 -0300

    feat(05-Interactive-Form-UI): [02/06] types/models

---
+++ 1-FrontEnd/7-Bootstrap/A - Bootstrap basico/05-Interactive-Form-UI/src/js/models.js
@@ -0,0 +1,26 @@
+export const FORM_FIELDS = [
+  {
+    id: "input-name",
+    type: "text",
+    label: "Nome Completo",
+    placeholder: "Digite seu nome",
+    required: true,
+    feedback: "Por favor, insira seu nome completo."
+  },
+  {
+    id: "input-email",
+    type: "email",
+    label: "Endereço de E-mail",
+    placeholder: "exemplo@aura.com",
+    required: true,
+    feedback: "Por favor, insira um e-mail corporativo válido."
+  },
+  {
+    id: "input-password",
+    type: "password",
+    label: "Senha de Acesso",
+    placeholder: "Mínimo 8 caracteres",
+    required: true,
+    feedback: "A senha deve conter pelo menos 8 caracteres."
+  }
+];
