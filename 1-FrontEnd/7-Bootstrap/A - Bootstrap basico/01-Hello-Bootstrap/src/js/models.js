e2738bc12e512625cf928a2ae7e9e09a684ff4fa
Cristiano Camacho <originalvgo@gmail.com>
2026-05-23 22:52:40 -0300

    feat(01-Hello-Bootstrap): [02/06] types/models

---
+++ 1-FrontEnd/7-Bootstrap/A - Bootstrap basico/01-Hello-Bootstrap/src/js/models.js
@@ -0,0 +1,26 @@
+export const COMPONENTS = [
+  {
+    name: "Botões",
+    type: "buttons",
+    description: "Elementos de ação estilizados com variações de cores e tamanhos nativos.",
+    icon: "🔘"
+  },
+  {
+    name: "Alertas",
+    type: "alerts",
+    description: "Caixas de mensagens contextuais dinâmicas com suporte a fechamento reativo.",
+    icon: "🚨"
+  },
+  {
+    name: "Spinners",
+    type: "spinners",
+    description: "Indicadores táteis de carregamento e processamento em segundo plano.",
+    icon: "🌀"
+  },
+  {
+    name: "Cards",
+    type: "cards",
+    description: "Contêineres de conteúdo flexíveis e extensíveis com cabeçalhos e rodapés.",
+    icon: "🎴"
+  }
+];
