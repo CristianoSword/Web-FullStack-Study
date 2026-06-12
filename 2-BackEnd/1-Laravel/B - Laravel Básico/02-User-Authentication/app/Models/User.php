d03a79ccd90e05d0ad9b8600eb71f4e26115a4dc
Cristiano Camacho <originalvgo@gmail.com>
2026-06-12 00:37:08 -0300

    [02/06] types/models: Add User model and migration
    - Add User model with Authenticatable trait
    - Add migration for users table with name, email, password
    - Configure password hashing via mutator
    - Add email_verified_at and remember_token fields
    - Implement AuthenticatableContract interface

---
+++ 2-BackEnd/1-Laravel/B - Laravel Básico/02-User-Authentication/app/Models/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+
+class User extends Model implements AuthenticatableContract
+{
+    use Authenticatable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+}
