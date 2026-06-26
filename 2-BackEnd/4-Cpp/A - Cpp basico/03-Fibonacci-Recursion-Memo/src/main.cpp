f607b453ad2fecbefa6c28c9849837b39312b88f
Cristiano Camacho <originalvgo@gmail.com>
2026-06-26 08:54:55 -0300

    03-Fibonacci-Recursion-Memo [04/06] ui/routes

--- 2-BackEnd/4-Cpp/A - Cpp basico/03-Fibonacci-Recursion-Memo/src/main.cpp
+++ 2-BackEnd/4-Cpp/A - Cpp basico/03-Fibonacci-Recursion-Memo/src/main.cpp
@@ -1,6 +1,16 @@
+#include "../include/fib_cache.hpp"
+#include "../include/fibonacci_service.hpp"
+
 #include <iostream>
 
-int main() {
-  std::cout << "fibonacci memo ready\n";
+int main(int argc, char** argv) {
+  int target = 10;
+
+  if (argc > 1) {
+    target = std::stoi(argv[1]);
+  }
+
+  FibCache cache;
+  std::cout << fibMemo(target, cache) << "\n";
   return 0;
 }
