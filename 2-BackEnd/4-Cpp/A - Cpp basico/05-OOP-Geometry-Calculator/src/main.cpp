b613bc0374056a597d36e1c917ee627fe375e496
Cristiano Camacho <originalvgo@gmail.com>
2026-06-26 10:56:14 -0300

    05-OOP-Geometry-Calculator [04/06] ui/routes

--- 2-BackEnd/4-Cpp/A - Cpp basico/05-OOP-Geometry-Calculator/src/main.cpp
+++ 2-BackEnd/4-Cpp/A - Cpp basico/05-OOP-Geometry-Calculator/src/main.cpp
@@ -1,6 +1,12 @@
+#include "../include/circle.hpp"
+#include "../include/rectangle.hpp"
+
 #include <iostream>
 
 int main() {
-  std::cout << "geometry ready\n";
+  Rectangle rectangle(4.0, 6.0);
+  Circle circle(3.0);
+  std::cout << "rectangle: " << rectangle.area() << "\n";
+  std::cout << "circle: " << circle.area() << "\n";
   return 0;
 }
