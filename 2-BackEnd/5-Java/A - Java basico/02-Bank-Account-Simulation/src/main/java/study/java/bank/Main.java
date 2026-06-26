78c320d1cbb48fcc39779b20a5a23f486221d375
Cristiano Camacho <originalvgo@gmail.com>
2026-06-26 18:28:39 -0300

    02-Bank-Account-Simulation [05/06] validation/fixes

--- 2-BackEnd/5-Java/A - Java basico/02-Bank-Account-Simulation/src/main/java/study/java/bank/Main.java
+++ 2-BackEnd/5-Java/A - Java basico/02-Bank-Account-Simulation/src/main/java/study/java/bank/Main.java
@@ -2,6 +2,11 @@ package study.java.bank;
 
 public class Main {
   public static void main(String[] args) {
+    TransferValidator validator = new TransferValidator();
+    if (!validator.isValid(120)) {
+      throw new IllegalArgumentException("invalid transfer");
+    }
+
     Account maria = new Account("Maria", 1000);
     Account joao = new Account("Joao", 500);
     TransferReceipt receipt = new BankService().transfer(maria, joao, 120);
---
+++ 2-BackEnd/5-Java/A - Java basico/02-Bank-Account-Simulation/src/main/java/study/java/bank/TransferValidator.java
@@ -0,0 +1,7 @@
+package study.java.bank;
+
+public class TransferValidator {
+  public boolean isValid(double amount) {
+    return amount > 0;
+  }
+}
