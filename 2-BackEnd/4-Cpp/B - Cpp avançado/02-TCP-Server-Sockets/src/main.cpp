9c9b7cc754daa90e809c58af15f96a8e6f26d5e8
Cristiano Camacho <originalvgo@gmail.com>
2026-06-26 13:05:16 -0300

    02-TCP-Server-Sockets [04/06] ui/routes

--- 2-BackEnd/4-Cpp/B - Cpp avançado/02-TCP-Server-Sockets/src/main.cpp
+++ 2-BackEnd/4-Cpp/B - Cpp avançado/02-TCP-Server-Sockets/src/main.cpp
@@ -1,6 +1,12 @@
+#include "../include/server_state.hpp"
+
 #include <iostream>
 
 int main() {
-  std::cout << "tcp server ready\n";
+  ServerState state;
+  state.push({"system", "server booted"});
+  for (const auto& message : state.history()) {
+    std::cout << message.author << ": " << message.body << "\n";
+  }
   return 0;
 }
