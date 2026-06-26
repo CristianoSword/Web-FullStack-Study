b4d8357fc657797a71fdbc7915d8e5f74f339970
Cristiano Camacho <originalvgo@gmail.com>
2026-06-26 18:21:57 -0300

    02-Bank-Account-Simulation [04/06] ui/routes

--- 2-BackEnd/5-Java/A - Java basico/02-Bank-Account-Simulation/src/main/java/study/java/bank/Main.java
+++ 2-BackEnd/5-Java/A - Java basico/02-Bank-Account-Simulation/src/main/java/study/java/bank/Main.java
@@ -2,6 +2,9 @@ package study.java.bank;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("bank ready");
+    Account maria = new Account("Maria", 1000);
+    Account joao = new Account("Joao", 500);
+    TransferReceipt receipt = new BankService().transfer(maria, joao, 120);
+    System.out.println(receipt.amount());
   }
 }
