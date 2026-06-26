dc7575332347a18ec6944078b5eb4abc86f70782
Cristiano Camacho <originalvgo@gmail.com>
2026-06-26 17:16:20 -0300

    01-Calculator-CLI [04/06] ui/routes

--- 2-BackEnd/5-Java/A - Java basico/01-Calculator-CLI/src/main/java/study/java/calculator/Main.java
+++ 2-BackEnd/5-Java/A - Java basico/01-Calculator-CLI/src/main/java/study/java/calculator/Main.java
@@ -2,6 +2,8 @@ package study.java.calculator;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("calculator ready");
+    CalculatorService service = new CalculatorService();
+    CalculationResult result = service.execute(Operation.ADD, 10, 5);
+    System.out.println(result.value());
   }
 }
