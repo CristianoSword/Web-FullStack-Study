758cdec9727cf7154ce6b41520214e71553572ca
Cristiano Camacho <originalvgo@gmail.com>
2026-06-25 21:26:59 -0300

    02-Student-Registry [02/06] types/models

---
+++ 2-BackEnd/4-Cpp/A - Cpp basico/02-Student-Registry/include/search_result.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "student.hpp"
+
+#include <optional>
+
+using StudentSearchResult = std::optional<Student>;
---
+++ 2-BackEnd/4-Cpp/A - Cpp basico/02-Student-Registry/include/student.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+struct Student {
+  std::string name;
+  double grade;
+};
