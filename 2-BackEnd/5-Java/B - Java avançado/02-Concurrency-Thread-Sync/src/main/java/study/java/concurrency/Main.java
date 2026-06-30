db6020537db544c399674b6debb4ca9694aac0fd
Cristiano Camacho <originalvgo@gmail.com>
2026-06-30 08:00:45 -0300

    02-Concurrency-Thread-Sync [05/06] validation/fixes

--- 2-BackEnd/5-Java/B - Java avançado/02-Concurrency-Thread-Sync/src/main/java/study/java/concurrency/Main.java
+++ 2-BackEnd/5-Java/B - Java avançado/02-Concurrency-Thread-Sync/src/main/java/study/java/concurrency/Main.java
@@ -2,6 +2,11 @@ package study.java.concurrency;
 
 public class Main {
   public static void main(String[] args) {
+    SyncValidator validator = new SyncValidator();
+    if (!validator.isValid(3)) {
+      throw new IllegalArgumentException("invalid increment amount");
+    }
+
     SyncSnapshot snapshot = new SyncCoordinator().incrementMany(3);
     System.out.println(snapshot.increments());
   }
---
+++ 2-BackEnd/5-Java/B - Java avançado/02-Concurrency-Thread-Sync/src/main/java/study/java/concurrency/SyncValidator.java
@@ -0,0 +1,7 @@
+package study.java.concurrency;
+
+public class SyncValidator {
+  public boolean isValid(int times) {
+    return times > 0;
+  }
+}
