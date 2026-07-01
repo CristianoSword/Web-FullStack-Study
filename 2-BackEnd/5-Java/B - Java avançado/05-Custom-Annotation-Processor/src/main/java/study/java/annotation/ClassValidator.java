3e108c00a71690d65e210452c294faaf9437338d
Cristiano Camacho <originalvgo@gmail.com>
2026-07-01 19:13:38 -0300

    05-Custom-Annotation-Processor [05/06] validation/fixes

---
+++ 2-BackEnd/5-Java/B - Java avançado/05-Custom-Annotation-Processor/src/main/java/study/java/annotation/ClassValidator.java
@@ -0,0 +1,7 @@
+package study.java.annotation;
+
+public class ClassValidator {
+  public boolean isScannable(Class<?> type) {
+    return type != null;
+  }
+}
--- 2-BackEnd/5-Java/B - Java avançado/05-Custom-Annotation-Processor/src/main/java/study/java/annotation/Main.java
+++ 2-BackEnd/5-Java/B - Java avançado/05-Custom-Annotation-Processor/src/main/java/study/java/annotation/Main.java
@@ -2,6 +2,10 @@ package study.java.annotation;
 
 public class Main {
   public static void main(String[] args) {
+    if (!new ClassValidator().isScannable(TrackedService.class)) {
+      throw new IllegalArgumentException("invalid class");
+    }
+
     TrackingResult result = new ReflectionScanner().scan(TrackedService.class);
     System.out.println(result.label());
   }
