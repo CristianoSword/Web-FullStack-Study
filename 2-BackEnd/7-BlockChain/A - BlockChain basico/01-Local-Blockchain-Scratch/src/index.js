8d94a302735ffa03f0fc38318490ad4c698707a4
Cristiano Camacho <originalvgo@gmail.com>
2026-07-14 13:18:53 -0300

    01-Local-Blockchain-Scratch [04/06] ui/routes

--- 2-BackEnd/7-BlockChain/A - BlockChain basico/01-Local-Blockchain-Scratch/src/index.js
+++ 2-BackEnd/7-BlockChain/A - BlockChain basico/01-Local-Blockchain-Scratch/src/index.js
@@ -1,4 +1,35 @@
 import { Blockchain } from "./services/blockchain.js";
 
 const blockchain = new Blockchain();
-console.log(`Blockchain bootstrapped with genesis hash ${blockchain.getLatestBlock().hash}`);
+const command = process.argv[2] ?? "help";
+
+switch (command) {
+  case "demo":
+    blockchain.addTransaction({ from: "alice", to: "bob", amount: 15, memo: "Invoice #1001" });
+    blockchain.addTransaction({ from: "carol", to: "dave", amount: 22, memo: "Payroll" });
+    console.log("Mining pending transactions...");
+    console.log(blockchain.minePendingTransactions("miner-01"));
+    break;
+  case "chain":
+    console.log(JSON.stringify(blockchain.state.chain, null, 2));
+    break;
+  case "add-tx":
+    blockchain.addTransaction({
+      from: process.argv[3],
+      to: process.argv[4],
+      amount: Number(process.argv[5]),
+      memo: process.argv[6] ?? ""
+    });
+    console.log(JSON.stringify(blockchain.state.pendingTransactions, null, 2));
+    break;
+  case "mine":
+    console.log(blockchain.minePendingTransactions(process.argv[3] ?? "miner-01"));
+    break;
+  default:
+    console.log("Commands:");
+    console.log("  npm run demo");
+    console.log("  npm run chain");
+    console.log("  node src/index.js add-tx <from> <to> <amount> [memo]");
+    console.log("  node src/index.js mine <minerAddress>");
+    break;
+}
