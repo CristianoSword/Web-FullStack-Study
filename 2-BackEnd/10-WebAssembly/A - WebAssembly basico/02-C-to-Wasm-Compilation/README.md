9c93d8a6ea500c88335c25caa49648c00a94dd34
Cristiano Camacho <originalvgo@gmail.com>
2026-08-07 08:03:36 -0300

    [01/06] setup

---
+++ 2-BackEnd/10-WebAssembly/A - WebAssembly basico/02-C-to-Wasm-Compilation/README.md
@@ -0,0 +1,3 @@
+# C to Wasm Compilation
+
+Source-complete WebAssembly project that compiles C source to `.wasm` with Emscripten.
---
+++ 2-BackEnd/10-WebAssembly/A - WebAssembly basico/02-C-to-Wasm-Compilation/build.bat
@@ -0,0 +1,4 @@
+@echo off
+setlocal
+if not exist dist mkdir dist
+emcc src\math_metrics.c -O2 -s WASM=1 -s MODULARIZE=1 -s EXPORT_ES6=1 -s EXPORTED_FUNCTIONS=["_sum_range","_weighted_average","_clamp_score"] -o dist\math_metrics.js
