1d0f5047f30a62b3d46bd098719a8f4023c85ed6
Cristiano Camacho <originalvgo@gmail.com>
2026-08-11 11:16:48 -0300

    [02/06] types/models

---
+++ 2-BackEnd/10-WebAssembly/A - WebAssembly basico/05-Wat-Text-Format-Handwritten/web/jobs.js
@@ -0,0 +1,6 @@
+export const watJobs = [
+  { label: "Add two values", exportName: "add_i32", args: [14, 29] },
+  { label: "Multiply values", exportName: "mul_i32", args: [7, 8] },
+  { label: "Triangular number", exportName: "triangular", args: [12] },
+  { label: "Absolute difference", exportName: "abs_diff", args: [18, 42] }
+];
---
+++ 2-BackEnd/10-WebAssembly/A - WebAssembly basico/05-Wat-Text-Format-Handwritten/web/module_exports.js
@@ -0,0 +1,6 @@
+export const exportedFunctions = {
+  add_i32: { arity: 2, description: "Adds two integers" },
+  mul_i32: { arity: 2, description: "Multiplies two integers" },
+  triangular: { arity: 1, description: "Computes the nth triangular number" },
+  abs_diff: { arity: 2, description: "Computes the absolute difference" }
+};
