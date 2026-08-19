b098653f6b6b9268462c9e4ae9d63a56e164d778
Cristiano Camacho <originalvgo@gmail.com>
2026-08-19 15:03:32 -0300

    [03/06] core-logic

---
+++ 2-BackEnd/11-Erlang/A - Erlang basico/05-Erlang-Rebar3-Setup/src/rebar3_setup_service.erl
@@ -0,0 +1,65 @@
+-module(rebar3_setup_service).
+-behaviour(gen_server).
+
+-export([
+    start_link/0,
+    bootstrap_report/0,
+    bootstrap_targets/0
+]).
+
+-export([
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3
+]).
+
+-include("rebar3_setup.hrl").
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+bootstrap_report() ->
+    gen_server:call(?MODULE, bootstrap_report).
+
+bootstrap_targets() ->
+    [
+        #bootstrap_target{name = app, src_dir = <<"src">>, module = rebar3_setup_app},
+        #bootstrap_target{name = supervisor, src_dir = <<"src">>, module = rebar3_setup_sup},
+        #bootstrap_target{name = service, src_dir = <<"src">>, module = rebar3_setup_service}
+    ].
+
+init([]) ->
+    {ok, #{
+        app_name => rebar3_setup,
+        targets => bootstrap_targets(),
+        notes => [
+            <<"rebar.config defines src_dirs and erl_opts">>,
+            <<"app.src wires the OTP application callback">>,
+            <<"service module can be started under a supervisor tree">>
+        ]
+    }}.
+
+handle_call(bootstrap_report, _From, State) ->
+    Report = #bootstrap_report{
+        app_name = maps:get(app_name, State),
+        targets = maps:get(targets, State),
+        notes = maps:get(notes, State)
+    },
+    {reply, Report, State};
+handle_call(_Request, _From, State) ->
+    {reply, {error, unsupported_call}, State}.
+
+handle_cast(_Message, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVersion, State, _Extra) ->
+    {ok, State}.
