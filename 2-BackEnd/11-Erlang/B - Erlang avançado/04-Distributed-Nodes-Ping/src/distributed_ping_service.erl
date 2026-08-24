2fae6a1d1a4d660b694755be4cbb23973c9bf10f
Cristiano Camacho <originalvgo@gmail.com>
2026-08-24 11:15:32 -0300

    [05/06] validation/fixes

--- 2-BackEnd/11-Erlang/B - Erlang avançado/04-Distributed-Nodes-Ping/src/distributed_ping_service.erl
+++ 2-BackEnd/11-Erlang/B - Erlang avançado/04-Distributed-Nodes-Ping/src/distributed_ping_service.erl
@@ -3,7 +3,8 @@
 
 -export([
     start_link/0,
-    run_demo/0
+    run_demo/0,
+    validate_report/1
 ]).
 
 -export([
@@ -15,13 +16,27 @@
     code_change/3
 ]).
 
+-include_lib("eunit/include/eunit.hrl").
 -include("distributed_ping.hrl").
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 run_demo() ->
-    gen_server:call(?MODULE, run_demo, 15000).
+    gen_server:call(?MODULE, run_demo, 60000).
+
+validate_report(Report) ->
+    case Report of
+        #cluster_report{peers = Peers, results = Results} when length(Peers) =:= 2, length(Results) =:= 2 ->
+            case lists:all(fun(#ping_result{status = Status}) -> Status =:= pong end, Results) of
+                true ->
+                    ok;
+                false ->
+                    {error, ping_failed}
+            end;
+        _ ->
+            {error, incomplete_cluster_report}
+    end.
 
 init([]) ->
     {ok, #{}}.
@@ -50,40 +65,38 @@ code_change(_OldVersion, State, _Extra) ->
 
 run_cluster_demo() ->
     ensure_origin_distribution(),
-    CookieArg = io_lib:format("-setcookie ~s", [atom_to_list(erlang:get_cookie())]),
-    {ok, AlphaPid, AlphaNode} = peer:start_link(#{
-        name => peer:random_name("alpha"),
-        wait_boot => 5000,
-        args => [lists:flatten(CookieArg)]
-    }),
+    {ok, AlphaNode, AlphaPort} = start_external_node("alpha_ping"),
     try
-        {ok, BetaPid, BetaNode} = peer:start_link(#{
-            name => peer:random_name("beta"),
-            wait_boot => 5000,
-            args => [lists:flatten(CookieArg)]
-        }),
+        {ok, BetaNode, BetaPort} = start_external_node("beta_ping"),
         try
-            AlphaTarget = #node_target{name = <<"alpha">>, full_node = AlphaNode, controller = AlphaPid},
-            BetaTarget = #node_target{name = <<"beta">>, full_node = BetaNode, controller = BetaPid},
+            AlphaTarget = #node_target{name = <<"alpha">>, full_node = AlphaNode, controller = AlphaPort},
+            BetaTarget = #node_target{name = <<"beta">>, full_node = BetaNode, controller = BetaPort},
             Results = [
-                make_ping_result(AlphaNode, BetaNode, peer:call(AlphaPid, net_adm, ping, [BetaNode])),
-                make_ping_result(BetaNode, AlphaNode, peer:call(BetaPid, net_adm, ping, [AlphaNode]))
+                make_ping_result(AlphaNode, BetaNode, rpc:call(AlphaNode, net_adm, ping, [BetaNode])),
+                make_ping_result(BetaNode, AlphaNode, rpc:call(BetaNode, net_adm, ping, [AlphaNode]))
             ],
-            {ok, #cluster_report{
+            Report = #cluster_report{
                 origin_node = node(),
                 peers = [AlphaTarget, BetaTarget],
                 results = Results
-            }}
+            },
+            case validate_report(Report) of
+                ok ->
+                    {ok, Report};
+                {error, Reason} ->
+                    {error, Reason}
+            end
         after
-            stop_peer_safe(BetaPid)
+            stop_node_safe(BetaNode, BetaPort)
         end
     after
-        stop_peer_safe(AlphaPid)
+        stop_node_safe(AlphaNode, AlphaPort)
     end.
 
 ensure_origin_distribution() ->
     case node() of
         nonode@nohost ->
+            ensure_epmd(),
             {ok, _} = net_kernel:start([origin_ping, shortnames]),
             ok;
         _ ->
@@ -93,8 +106,133 @@ ensure_origin_distribution() ->
 make_ping_result(From, To, Status) ->
     #ping_result{from = From, to = To, status = Status}.
 
-stop_peer_safe(undefined) ->
+start_external_node(Prefix) ->
+    Host = hostname(),
+    Suffix = integer_to_list(erlang:unique_integer([positive])),
+    Name = Prefix ++ "_" ++ Suffix,
+    Node = list_to_atom(Name ++ "@" ++ Host),
+    Cookie = atom_to_list(erlang:get_cookie()),
+    Erl = erl_path(),
+    Port = open_port(
+        {spawn_executable, Erl},
+        [
+            hide,
+            exit_status,
+            {args, ["-sname", Name, "-setcookie", Cookie, "-noshell", "-eval", "receive after infinity -> ok end."]}
+        ]
+    ),
+    case wait_for_node(Node, 80) of
+        {ok, _} ->
+            {ok, Node, Port};
+        {error, Reason} ->
+            port_close_safe(Port),
+            {error, Reason}
+    end.
+
+wait_for_node(_Node, 0) ->
+    {error, node_boot_timeout};
+wait_for_node(Node, AttemptsLeft) ->
+    timer:sleep(100),
+    case net_adm:ping(Node) of
+        pong ->
+            {ok, Node};
+        pang ->
+            wait_for_node(Node, AttemptsLeft - 1)
+    end.
+
+hostname() ->
+    {ok, Host} = inet:gethostname(),
+    Host.
+
+erl_path() ->
+    [Path | _] = filelib:wildcard(filename:join([code:root_dir(), "erts-*", "bin", "erl.exe"])),
+    Path.
+
+stop_node_safe(undefined, _Port) ->
     ok;
-stop_peer_safe(Pid) when is_pid(Pid) ->
-    catch peer:stop(Pid),
+stop_node_safe(Node, Port) when is_atom(Node) ->
+    catch rpc:call(Node, erlang, halt, []),
+    wait_for_node_down(Node, 40),
+    port_close_safe(Port),
     ok.
+
+wait_for_node_down(_Node, 0) ->
+    ok;
+wait_for_node_down(Node, AttemptsLeft) ->
+    timer:sleep(100),
+    case net_adm:ping(Node) of
+        pang ->
+            ok;
+        pong ->
+            wait_for_node_down(Node, AttemptsLeft - 1)
+    end.
+
+ensure_epmd() ->
+    case epmd_path() of
+        undefined ->
+            ok;
+        Path ->
+            os:cmd("cmd /c start \"\" \"" ++ Path ++ "\""),
+            timer:sleep(500),
+            ok
+    end.
+
+epmd_path() ->
+    case filelib:wildcard(filename:join([code:root_dir(), "erts-*", "bin", "epmd.exe"])) of
+        [Path | _] ->
+            Path;
+        [] ->
+            undefined
+    end.
+
+port_close_safe(Port) when is_port(Port) ->
+    catch port_close(Port),
+    ok;
+port_close_safe(_Port) ->
+    ok.
+
+validate_report_test() ->
+    GoodReport = #cluster_report{
+        origin_node = 'origin_ping@host',
+        peers = [
+            #node_target{name = <<"alpha">>, full_node = 'alpha@host', controller = self()},
+            #node_target{name = <<"beta">>, full_node = 'beta@host', controller = self()}
+        ],
+        results = [
+            #ping_result{from = 'alpha@host', to = 'beta@host', status = pong},
+            #ping_result{from = 'beta@host', to = 'alpha@host', status = pong}
+        ]
+    },
+    ?assertEqual(ok, validate_report(GoodReport)).
+
+invalid_report_test() ->
+    ?assertEqual(
+        {error, incomplete_cluster_report},
+        validate_report(#cluster_report{origin_node = node(), peers = [], results = []})
+    ),
+    BadReport = #cluster_report{
+        origin_node = node(),
+        peers = [
+            #node_target{name = <<"alpha">>, full_node = 'alpha@host', controller = self()},
+            #node_target{name = <<"beta">>, full_node = 'beta@host', controller = self()}
+        ],
+        results = [
+            #ping_result{from = 'alpha@host', to = 'beta@host', status = pang},
+            #ping_result{from = 'beta@host', to = 'alpha@host', status = pong}
+        ]
+    },
+    ?assertEqual({error, ping_failed}, validate_report(BadReport)).
+
+report_shape_test() ->
+    Report = #cluster_report{
+        origin_node = node(),
+        peers = [
+            #node_target{name = <<"alpha">>, full_node = 'alpha@host', controller = external},
+            #node_target{name = <<"beta">>, full_node = 'beta@host', controller = external}
+        ],
+        results = [
+            #ping_result{from = 'alpha@host', to = 'beta@host', status = pong},
+            #ping_result{from = 'beta@host', to = 'alpha@host', status = pong}
+        ]
+    },
+    ?assertEqual(ok, validate_report(Report)).
