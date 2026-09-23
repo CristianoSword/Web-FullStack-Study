31cd52a342238771f47b5d1577048f3641e76638
Cristiano Camacho <originalvgo@gmail.com>
2026-09-23 14:23:16 -0300

    [02/06] types/models

---
+++ 2-BackEnd/15-GraphQL/B - GraphQL avançado/03-GraphQL-Subscriptions-Websockets/src/config/server-config.js
@@ -0,0 +1,6 @@
+export const serverConfig = {
+  port: 4203,
+  graphqlEndpoint: "/graphql",
+  wsPath: "/graphql",
+  appName: "graphql-subscriptions-websockets"
+};
---
+++ 2-BackEnd/15-GraphQL/B - GraphQL avançado/03-GraphQL-Subscriptions-Websockets/src/models/live-score-store.js
@@ -0,0 +1,10 @@
+export const liveScoreStore = {
+  latestScore: {
+    matchId: "MTC-1001",
+    homeTeam: "OpenAI FC",
+    awayTeam: "Codex United",
+    homeScore: 0,
+    awayScore: 0,
+    minute: 0
+  }
+};
